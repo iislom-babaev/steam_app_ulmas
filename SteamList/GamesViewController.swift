7ad1c16f7f0e6083b860f94d6982bc674ebc47fe
u.rakhmatullaev <u.rakhmatullayev@uic.group>
2022-12-27 17:10:26 +0500

    ref: link class files to storyboard views

---
+++ SteamList/GamesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GamesViewController.swift
+//  SteamList
+//
+//  Created by Macbook on 27/12/2022.
+//
+
+import UIKit
+
+class GamesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
---
+++ SteamList/NewsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewsViewController.swift
+//  SteamList
+//
+//  Created by Macbook on 27/12/2022.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
