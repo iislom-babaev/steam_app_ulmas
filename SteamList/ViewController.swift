1810b5155a8e21098082f6c72b6b30deff795e72
u.rakhmatullaev <u.rakhmatullayev@uic.group>
2022-12-27 10:54:01 +0500

    Initial Commit

---
+++ SteamList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SteamList
+//
+//  Created by Macbook on 27/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
