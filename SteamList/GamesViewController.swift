f1e9c5bf7040d5200a9cd27631a8d3c4ddcfb1cd
u.rakhmatullaev <u.rakhmatullayev@uic.group>
2022-12-28 13:17:06 +0500

    feat: add tableview of games

--- SteamList/GamesViewController.swift
+++ SteamList/GamesViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class GamesViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    private let numberOfGames = 100
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
 
 
+}
+
+extension GamesViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numberOfGames
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for:  indexPath)
+        cell.textLabel?.text = "Game \(indexPath.row)"
+        cell.textLabel?.textColor = .white
+        return cell
+    }
+    
+
+    
+    
+    
+}
+
+extension GamesViewController: UITableViewDelegate {
+    
 }
