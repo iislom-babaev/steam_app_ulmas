ade6aac1a8935ce9e38275b53bddc627520fd22b
u.rakhmatullaev <u.rakhmatullayev@uic.group>
2022-12-29 12:27:24 +0500

    feat: register custom cell into tableview

--- SteamList/GamesViewController.swift
+++ SteamList/GamesViewController.swift
@@ -10,13 +10,25 @@ import UIKit
 class GamesViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-    private let numberOfGames = 100
+    
+    let cellId = "GameTableViewCell"
+    private var games: [Game] = [
+    Game(title: "Dota 2", isFavorite: false),
+    Game(title: "League of Legends", isFavorite: false),
+    Game(title: "GTA Vice City", isFavorite: false),
+    Game(title: "Counter Strkie 1.6", isFavorite: false),
+    Game(title: "Need For Speed", isFavorite: false)
+    ]
+        
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.delegate = self
         tableView.dataSource = self
+        
+        tableView.register(UINib.init(nibName: cellId, bundle: nil), forCellReuseIdentifier: cellId)
     }
     
 
@@ -26,23 +38,37 @@ class GamesViewController: UIViewController {
 extension GamesViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return numberOfGames
+        return games.count
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for:  indexPath)
-        cell.textLabel?.text = "Game \(indexPath.row)"
-        cell.textLabel?.textColor = .white
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for:  indexPath) as! GameTableViewCell
+        let game = games[indexPath.row]
+        cell.cellTitle.text = "\(game.title)"
+        if game.isFavorite {
+            cell.cellIcon.imageView?.image = UIImage(systemName: "star.fill")
+        } else {
+            cell.cellIcon.imageView?.image = UIImage(systemName: "star")
+        }
+        
         return cell
     }
-    
-
-    
     
     
 }
 
 extension GamesViewController: UITableViewDelegate {
     
+}
+
+
+struct Game {
+    let title: String
+    var isFavorite: Bool
+    
+    init(title: String, isFavorite: Bool) {
+        self.title = title
+        self.isFavorite = isFavorite
+    }
 }
