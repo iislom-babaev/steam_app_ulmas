ba358b748876df9a5f27b0754f324e33b7cd4471
u.rakhmatullaev <rakhmatullaevulmas@gmail.com>
2023-01-06 17:01:54 +0500

    style: change navbar and cell selection styles

--- SteamList/Views/FavoritesTableViewCell.swift
+++ SteamList/Views/FavoritesTableViewCell.swift
@@ -14,6 +14,7 @@ class FavoritesTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        selectionStyle = .none
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
