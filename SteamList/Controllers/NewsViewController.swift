5b4ad550d71cd43a29002fcf5fb5bb2aa5b5ea3c
u.rakhmatullaev <rakhmatullaevulmas@gmail.com>
2023-01-06 16:14:07 +0500

    add tableview

--- SteamList/Controllers/NewsViewController.swift
+++ SteamList/Controllers/NewsViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class NewsViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
---
+++ SteamList/Views/NewsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewsTableViewCell.swift
+//  SteamList
+//
+//  Created by Macbook on 06/01/2023.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
