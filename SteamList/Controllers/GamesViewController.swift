a128be5260361b212d06626e07c76d83083e8976
u.rakhmatullaev <rakhmatullaevulmas@gmail.com>
2023-01-17 12:45:09 +0500

    feat: make table view scrollable under the keyboard

--- SteamList/Controllers/GamesViewController.swift
+++ SteamList/Controllers/GamesViewController.swift
@@ -32,6 +32,18 @@ class GamesViewController: UIViewController{
         searchController.searchResultsUpdater = self
         navigationItem.searchController = searchController
         navigationController?.hideKeyboardWhenTappedAround()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc private func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height + tableView.rowHeight, right: 0)
+        }
+    }
+    
+    @objc private func keyboardWillHide(notification: NSNotification) {
+        tableView.contentInset = .zero
     }
 }
 
