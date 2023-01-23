bf626a379a2b31037bb58a51d7eb2d40f50c5752
u.rakhmatullaev <rakhmatullaevulmas@gmail.com>
2023-01-23 12:22:10 +0500

    code: add news mock data

--- SteamList/Data/MockData.swift
+++ SteamList/Data/MockData.swift
@@ -26,4 +26,11 @@ struct MockData {
     Game(title: "GTA Vice City", isFavorite: false),
     Game(title: "GTA Vice City", isFavorite: false)
     ]
+    
+    static let news: [News] = [
+        News(title: "Best PC Games of 2022", gameName: "Dota 2", developer: "Valve", date: "1 Nov, 2022"),
+        News(title: "Tournament Winner!", gameName: "CS: GO", developer: "Gearbox Crew", date: "12 March, 2022"),
+        News(title: "New Antimage Strategy", gameName: "Dota 2", developer: "Valve", date: "1 Dec, 2022"),
+        News(title: "Don't Miss Our Halloween Sale!", gameName: "Civilization 4", developer: "Software Publishers", date: "29 Oct, 2020"),
+    ]
 }
